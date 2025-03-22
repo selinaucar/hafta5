c812b7bb8ed490a7dc6ad19b203fa7abbd86b1d4
selinaucar <selina_ucar@hotmail.com>
2025-03-22 17:02:51 +0300

    first commit

--- h5oop.py
+++ h5oop.py
@@ -1,27 +1,31 @@
-class hesap:
-        def __init__(self, say1, say2):
-        self.say1= say1
+class Hesap:
+    def __init__(self, say1, say2):
+        self.say1 = say1
         self.say2 = say2
-        def carp (self):
-            carpma sonuc=self.say1 * self.say2
-            return sonuc
-        def bol(self):
-            bolme sonuc=return self.say1 / self.say2
-            return sonuc
-        def topla(self):
-            toplama sonuc=self.say1 + self.say2
-            return sonuc
-        def cikar(self):
-            cikarma sonuc=self.say1 - self.say2
-            return sonuc
 
-        def yazdir(self):
-            toplam= self.topla()
-            carpma= self.carp()
-            cikarma= self.cikar()
-            bolme= self.bol()
+    def carp(self):
+        sonuc = self.say1 * self.say2
+        return sonuc
 
-            print('sayilarin toplami:', toplam)
-            print('sayilarin carpimi:', carpma)
-            print('sayilarin cikarmami:', cikarma)
-            print('sayilarin bolme:', toplam)
+    def bol(self):
+        sonuc = self.say1 / self.say2
+        return sonuc
+
+    def topla(self):
+        sonuc = self.say1 + self.say2
+        return sonuc
+
+    def cikar(self):
+        sonuc = self.say1 - self.say2
+        return sonuc
+
+    def yazdir(self):
+        toplam = self.topla()
+        carpma = self.carp()
+        cikarma = self.cikar()
+        bolme = self.bol()
+
+        print('Sayilarin toplami:', toplam)
+        print('Sayilarin carpimi:', carpma)
+        print('Sayilarin cikarmasi:', cikarma)
+        print('Sayilarin bolme sonucu:', bolme)
